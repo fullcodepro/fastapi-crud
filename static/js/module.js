6eddecff79d1c3e911bf9ee9263ddfebc370c9d7
aalesann <alejandrosanabria74@gmail.com>
2023-06-06 10:57:56 -0300

    Animación de conversación

--- static/js/module.js
+++ static/js/module.js
@@ -1,15 +1,42 @@
 const btnEnviar = document.querySelector('#btn-enviar');
 const historyList = document.querySelector('#history-list');
 
+// const mostrarRespuesta = (respuesta) => {
+//     const li = document.createElement('li');
+//     li.classList.add('list-item', 'd-flex', 'justify-content-center', 'align-items-center', 'text-center', 'px-3', 'respuesta');
+//     const p = document.createElement('p');
+//     p.innerText = respuesta;
+//     li.appendChild(p);
+//     historyList.appendChild(li);
+//     scrollToBottom(historyList);
+// }
+
 const mostrarRespuesta = (respuesta) => {
     const li = document.createElement('li');
-    li.classList.add('list-item', 'd-flex', 'justify-content-center', 'align-items-center', 'text-center', 'px-3', 'respuesta');
-    const p = document.createElement('p');
-    p.innerText = respuesta;
-    li.appendChild(p);
+    li.classList.add('list-item', 'd-flex', 'align-items-center', 'px-3', 'respuesta');
+  
+    const respuestaContainer = document.createElement('span');
+    respuestaContainer.classList.add('respuesta-container', 'respuesta');
+    li.appendChild(respuestaContainer);
+  
     historyList.appendChild(li);
     scrollToBottom(historyList);
-}
+  
+    let index = 0;
+  const mostrarSiguienteLetra = () => {
+    if (index < respuesta.length) {
+      const textoParcial = respuesta.substring(0, index + 1);
+      respuestaContainer.textContent = textoParcial;
+      index++;
+      // Ajusta el tiempo entre cada letra (en milisegundos) para controlar la velocidad de la respuesta
+      const tiempoEntreLetras = 25; // Puedes ajustar este valor según tus necesidades
+      setTimeout(mostrarSiguienteLetra, tiempoEntreLetras);
+    }
+  };
+  
+    mostrarSiguienteLetra();
+  };
+  
 
 const mostrarPregunta = (pregunta) => {
     const li = document.createElement('li');
